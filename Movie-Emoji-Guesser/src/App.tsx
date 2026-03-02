8d94713dce3e064c8554c5eccb523cc9fbabd4e7
Grace Kangausaru <Grace.Kangausaru@altron.com>
2026-03-02 11:22:27 +0200

    Added Nav and footer and styling

--- Movie-Emoji-Guesser/src/App.tsx
+++ Movie-Emoji-Guesser/src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
-import {Nav} from "./components/Nav.jsx"
+import './App.css';
+import {Nav} from './components/Nav.js';
+import {Footer} from './components/Footer.js';
 
 function App() {
   
   return (
     <>
+   
     <Nav />
+    <div className = "text-4xl">
+      Sfundo's component comes here
+    </div>
+    <Footer />
     </>
   )
 }
