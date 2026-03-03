9031aeca29c96d80cf96d2595ae55e2f41aeed22
SudoSfundo <sfundokhumalo11@gmail.com>
2026-03-03 14:28:12 +0200

    trying to fix

--- Movie-Emoji-Guesser/src/App.tsx
+++ Movie-Emoji-Guesser/src/App.tsx
@@ -3,6 +3,7 @@ import {Nav} from './components/Nav.js';
 import {Footer} from './components/Footer.js';
 import MovieWrapper from './components/MovieWrapper.js';
 
+
 function App() {
   
 
@@ -13,8 +14,9 @@ function App() {
     <div className = "text-4xl">
       <MovieWrapper labelText='Insert Name' inputName='userName' actionHandler={()=>console.log("hi testing s")}></MovieWrapper>
     </div>
-    <Footer />
-    </>
+   <div  className = "justify-self-end "><Footer /></div>
+    </div>
+  </div>
   )
 }
 
