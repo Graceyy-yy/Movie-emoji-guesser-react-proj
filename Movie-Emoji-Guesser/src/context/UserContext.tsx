0235bf47f1c7ac40c3b913ade3abd3813aad68d3
Grace Kangausaru <Grace.Kangausaru@altron.com>
2026-03-06 09:25:39 +0200

    Fixed

--- Movie-Emoji-Guesser/src/context/UserContext.tsx
+++ Movie-Emoji-Guesser/src/context/UserContext.tsx
@@ -1,22 +1,17 @@
-import { createContext, useRef, useState, type ReactNode } from 'react';
+import { createContext, useState, type ReactNode } from 'react';
 import {type user} from '../types/user';
-
 
 const UserContext =createContext<user |undefined>(undefined);
 
 export function UserProvider({ children }:{children:ReactNode}){
-    const [name, setName] = useState<user>({name : ''});
+    const [name, setName] = useState<string>('');
 
-   // function login(name:string){setName({name})}
+    function login(name:string){setName(name)}
 
     const value:user = {name,login}
 
     return (
-<<<<<<< HEAD
-        <UserContext.Provider value={{name}}>
-=======
         <UserContext.Provider value={value}>
->>>>>>> de8c9f91f91e22169d0f239f192dd2a350bf3b95
             {children}
         </UserContext.Provider>
     )
