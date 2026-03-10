ff9f858f245a7c29e0aeeba835af1066a94568bc
SudoSfundo <sfundokhumalo11@gmail.com>
2026-03-10 09:55:28 +0200

    added movie list

--- Movie-Emoji-Guesser/src/components/MovieList.tsx
+++ Movie-Emoji-Guesser/src/components/MovieList.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import MoviesDataset from '../data/movies.json'
+import type { MovieType } from '../types/MovieType'
 
+const movies= MoviesDataset as MovieType[];
 
 export const MovieList = () => {
   return (
     <div className= 'text-4xl flex-1'>
-        
+        {movies.map((movie)=>(
+          
+          <li key={movie.id}>
+        <div>
+            <img src={`../src/images/${movie.answer}.jpg`} alt={`${movie.answer} image`}/>
+            <h2>{movie.answer}</h2>
+            <p>{movie.emojis}</p>
+            </div>
+            <button>Edit Movie Details</button>
+            <button>Delete Movie</button>
+          </li>
+         
+        ))}
     </div>
   )
 }
